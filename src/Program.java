1fe1955c8ef0ec63bf95a3a497f5e5387c6b44f0
Alan Tse <starttodevelop@gmail.com>
2017-05-10 10:54:19 +1000

    Initial Commit

---
+++ src/Program.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author tsea
+ *
+ */
+public class Program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
